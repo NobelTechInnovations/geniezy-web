d535ddf526e41c722a65f504a4dab6e77a738671
Kartik Maandothiya <kartikya27@gmail.com>
2025-06-04 21:57:18 +0530

    kayout

--- middleware.js
+++ middleware.js
@@ -1,15 +1,22 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  if (request.nextUrl.pathname === '/login') {
-    const isLoggedIn = request.cookies.get('isLoggedIn')?.value;
-    if (isLoggedIn === 'true') {
-      return NextResponse.redirect(new URL('/', request.url));
-    }
+  const currentUser = request.cookies.get('token')?.value;
+  const checkoutPath = '/checkout';
+  const loginPath = '/login';
+
+  if (request.nextUrl.pathname === checkoutPath && !currentUser) {
+    const loginUrl = new URL(loginPath, request.url);
+    return NextResponse.redirect(loginUrl);
   }
+
+  if (request.nextUrl.pathname === loginPath && currentUser) {
+     return NextResponse.redirect(new URL('/checkout', request.url)); 
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/login'],
+  matcher: ['/checkout', '/login'], 
 }; 
