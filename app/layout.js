13e8221e268db0bf7a7e4be586c68f0af590206a
Kartik Mandothiya <55804679+kartikya27@users.noreply.github.com>
2025-07-22 15:45:21 +0530

    ads

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta name="google-adsense-account" content="ca-pub-9809230558529342">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
