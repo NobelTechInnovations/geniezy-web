c41e801437e98ba2229d3bee660125f61d8ce000
Kartik Mandothiya <55804679+kartikya27@users.noreply.github.com>
2025-07-23 11:25:26 +0530

    bugs

--- app/layout.js
+++ app/layout.js
@@ -31,7 +31,6 @@ export default function RootLayout({ children }) {
       >
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9809230558529342"
      crossorigin="anonymous"></script>
-<!-- first -->
 <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-9809230558529342"
