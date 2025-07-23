9acc7974c70f8f5d70bc8bbd9ca60a9bd551db36
Kartik Mandothiya <55804679+kartikya27@users.noreply.github.com>
2025-07-23 11:22:48 +0530

    first ads

--- app/layout.js
+++ app/layout.js
@@ -29,6 +29,18 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9809230558529342"
+     crossorigin="anonymous"></script>
+<!-- first -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9809230558529342"
+     data-ad-slot="5541942961"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
         {children}
       </body>
     </html>
