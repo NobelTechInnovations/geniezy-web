29e978b0977140c9aa3d168659b8dcab3f88b6eb
Kartik Mandothiya <55804679+kartikya27@users.noreply.github.com>
2026-06-19 21:47:49 +0530

    useSearchParams

--- app/delivery-now/grocery-fresh/page.js
+++ app/delivery-now/grocery-fresh/page.js
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect } from 'react';
 import HorizontalCategoryList from '../../components/delivery/HorizontalCategoryList';
 import HorizontalProductList from '../../components/delivery/HorizontalProductList';
